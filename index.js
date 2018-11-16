dd8d7d8c518e2f38b4b7f4b6e28cff10de9beb41
Karolis Stulgys <k.stulgys@gmail.com>
2018-11-16 16:02:37 +1100

    changed server for heroku

--- index.js
+++ index.js
@@ -68,9 +68,13 @@ const server = new ApolloServer({
 
 // server.applyMiddleware({ app, path: '/graphql' })
 
-server.listen().then(({ url }) => {
-	console.log(`🚀 Server ready at ${url}`)
-})
+// server.listen().then(({ url }) => {
+// 	console.log(`🚀 Server ready at ${url}`)
+// })
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
 
 // app.listen({ port: 8000 }, () => {
 // 	console.log('Apollo Server on http://localhost:4000/graphql')
